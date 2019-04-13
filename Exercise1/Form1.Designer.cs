fbcecc64a39adff63200009a426566a75f132cdd
pearlinejomalon <43321994+pearlinejomalon@users.noreply.github.com>
2019-04-13 15:00:19 -0500

    Add files via upload

---
+++ Exercise1/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Exercise1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.closeButton = new System.Windows.Forms.Button();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.birthdateLabel = new System.Windows.Forms.Label();
+            this.genderLabel = new System.Windows.Forms.Label();
+            this.titleComboBox = new System.Windows.Forms.ComboBox();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.birthdatePicker = new System.Windows.Forms.DateTimePicker();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(12, 395);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(330, 395);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 1;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.firstNameLabel.Location = new System.Drawing.Point(33, 144);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(85, 18);
+            this.firstNameLabel.TabIndex = 2;
+            this.firstNameLabel.Text = "First Name:";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.lastNameLabel.Location = new System.Drawing.Point(34, 190);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(84, 18);
+            this.lastNameLabel.TabIndex = 3;
+            this.lastNameLabel.Text = "Last Name:";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.titleLabel.Location = new System.Drawing.Point(79, 104);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(39, 18);
+            this.titleLabel.TabIndex = 4;
+            this.titleLabel.Text = "Title:";
+            // 
+            // birthdateLabel
+            // 
+            this.birthdateLabel.AutoSize = true;
+            this.birthdateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.birthdateLabel.Location = new System.Drawing.Point(48, 235);
+            this.birthdateLabel.Name = "birthdateLabel";
+            this.birthdateLabel.Size = new System.Drawing.Size(70, 18);
+            this.birthdateLabel.TabIndex = 5;
+            this.birthdateLabel.Text = "Birthdate:";
+            // 
+            // genderLabel
+            // 
+            this.genderLabel.AutoSize = true;
+            this.genderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.genderLabel.Location = new System.Drawing.Point(57, 280);
+            this.genderLabel.Name = "genderLabel";
+            this.genderLabel.Size = new System.Drawing.Size(61, 18);
+            this.genderLabel.TabIndex = 6;
+            this.genderLabel.Text = "Gender:";
+            // 
+            // titleComboBox
+            // 
+            this.titleComboBox.FormattingEnabled = true;
+            this.titleComboBox.Items.AddRange(new object[] {
+            "Mr.",
+            "Mrs.",
+            "Ms."});
+            this.titleComboBox.Location = new System.Drawing.Point(155, 101);
+            this.titleComboBox.Name = "titleComboBox";
+            this.titleComboBox.Size = new System.Drawing.Size(121, 21);
+            this.titleComboBox.TabIndex = 7;
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(155, 142);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(196, 20);
+            this.firstNameTextBox.TabIndex = 8;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(155, 188);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(196, 20);
+            this.lastNameTextBox.TabIndex = 9;
+            // 
+            // birthdatePicker
+            // 
+            this.birthdatePicker.Location = new System.Drawing.Point(155, 233);
+            this.birthdatePicker.MaxDate = new System.DateTime(2018, 12, 31, 0, 0, 0, 0);
+            this.birthdatePicker.MinDate = new System.DateTime(1900, 1, 1, 0, 0, 0, 0);
+            this.birthdatePicker.Name = "birthdatePicker";
+            this.birthdatePicker.Size = new System.Drawing.Size(200, 20);
+            this.birthdatePicker.TabIndex = 10;
+            this.birthdatePicker.Value = new System.DateTime(2018, 12, 31, 0, 0, 0, 0);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.checkedListBox1.Location = new System.Drawing.Point(155, 280);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(101, 34);
+            this.checkedListBox1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(78, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 24);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Please enter your information:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 430);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.birthdatePicker);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.titleComboBox);
+            this.Controls.Add(this.genderLabel);
+            this.Controls.Add(this.birthdateLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.closeButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Personal Information";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label birthdateLabel;
+        private System.Windows.Forms.Label genderLabel;
+        private System.Windows.Forms.ComboBox titleComboBox;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.DateTimePicker birthdatePicker;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
